68f137d5e93f5455a0e99a4a0300cccc6ce132aa
Igor T <igor.trushevsky@gmail.com>
2019-12-10 23:50:33 -0500

    make shapes more rounded

--- src/components/ParallaxScene.jsx
+++ src/components/ParallaxScene.jsx
@@ -35,11 +35,15 @@ const ParallaxScene = ({ ...props }) => {
         .map((_, i) => (
           <div
             data-depth={Math.random()}
-            css={{ height: "100%", width: "100%" }}
+            css={{
+              height: "100%",
+              width: "100%",
+            }}
           >
             <div
               css={{
                 position: "absolute",
+                borderRadius: Math.random() * 50 + "%",
                 width: 10 * i,
                 height: 10 * i,
                 top: Math.random() * 100 + "%",
@@ -48,7 +52,7 @@ const ParallaxScene = ({ ...props }) => {
                   Math.floor(Math.random() * 256)
                 )}, ${Math.random()})`,
                 animation: `${anim} ${Math.max(
-                  Math.random() * 100,
+                  Math.random() * 50,
                   10
                 )}s ease infinite`,
               }}
@@ -60,10 +64,14 @@ const ParallaxScene = ({ ...props }) => {
         .map((_, i) => (
           <div
             data-depth={Math.random()}
-            css={{ height: "100%", width: "100%" }}
+            css={{
+              height: "100%",
+              width: "100%",
+            }}
           >
             <div
               css={{
+                borderRadius: Math.random() * 50 + "%",
                 position: "absolute",
                 width: 10 * i,
                 height: 10 * i,
@@ -74,7 +82,7 @@ const ParallaxScene = ({ ...props }) => {
                 )}, ${Math.random()})`,
                 animation: `${anim} ${Math.max(
                   Math.random() * 100,
-                  10
+                  30
                 )}s ease infinite`,
               }}
             ></div>
